43e22cdeb46837fa763f10c0f19e5160abb88940
Scott M <gmat191@gmail.com>
2018-10-15 03:56:33 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -49,7 +49,7 @@ def get_contestant_name(data, occupation)
     arr.each do |contestants|
       contestants.each do |k,v|
         if v == occupation
-          
+          return 
 end
 
 def count_contestants_by_hometown(data, hometown)
