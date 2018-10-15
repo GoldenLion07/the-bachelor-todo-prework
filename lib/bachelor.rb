58ddeee26ec97bc769fe0057bec9b66a5a1588d5
Scott M <gmat191@gmail.com>
2018-10-15 03:50:17 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -34,9 +34,9 @@ data = {
 }
 
 def get_first_name_of_season_winner(data, season)
-  data[season.to_sym].each do |into_arr|
-    if into_arr[:status] == "Winner"
-      return into_arr[:name].split(" ")[0]
+  data[season.to_sym].each do |into_array|
+   if into_array[:status] =="Winner"
+     return into_array[:name].split(" ")[0]
     end
   end
 end
