40f362ad50c2343ebc02d0dd0e805634dbd555f5
Scott M <gmat191@gmail.com>
2018-10-15 03:55:43 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -45,7 +45,8 @@ def get_first_name_of_season_winner(data, season)
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, arr|
+    
 end
 
 def count_contestants_by_hometown(data, hometown)
