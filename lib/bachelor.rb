dab37dcda77eafd21ce91ca932b760472ee56711
Scott M <gmat191@gmail.com>
2018-10-15 04:08:15 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,102 @@
+data = { 
+  "season 30": [
+    {
+      "name":      "Beth Smalls",
+      "age":       "26",
+      "hometown":  "Great Falls, Virginia",
+      "occupation":"Nanny/Freelance Journalist",
+      "status":    "Winner"
+    },
+    {
+      "name":       "Becca Tilley",
+      "age":        "27",
+      "hometown":   "Shreveport, Louisiana",
+      "occupation": "Chiropractic Assistant",
+      "status":     "Eliminated Week 8"
+    }
+  ],
+  "season 29": [
+    {
+      "name":      "Ashley Yeats",
+      "age":       "24",
+      "hometown":  "Denver, Colorado",
+      "occupation":"Dental Assitant",
+      "status":    "Winner"
+    },
+    {
+      "name":       "Sam Grover",
+      "age":        "29",
+      "hometown":   "New York, New York",
+      "occupation": "Entertainer",
+      "status":     "Eliminated Week 6"
+    }
+  ]
+}
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |contestants|
+    contestants.each do |k,v|
+      if v == "Winner"
+        full_name = contestants["name"]
+        return full_name.split(" ").first
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, arr|
+    arr.each do |contestants|
+      contestants.each do |k,v|
+        if v == occupation
+          return contestants["name"]
+        end
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  i = 0 
+  data.each do |season, arr|
+    arr.each do |hash|
+      hash.each do |k,v|
+        if v == hometown 
+          i += 1
+        end
+      end
+    end
+  end
+  i
+    
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, arr|
+    arr.each do |hash|
+      hash.each do |k,v|
+        if v == hometown
+          return hash["occupation"]
+        end
+      end
+    end
+  end
 end
+
 
 def get_average_age_for_season(data, season)
   # code here
+age = 0
+count = 0
+  data[season].each do |hash|
+    hash.each do |k,v|
+      if k == "age"
+        count += 1
+        age += v.to_f
+      end
+    end
+  end
+
+  answer = (age/count).round
+return answer
 end
