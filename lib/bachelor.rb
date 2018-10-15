3f9ca181a4beb77e6aef55e5e183a3a0ce040d86
Scott M <gmat191@gmail.com>
2018-10-15 03:55:05 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -37,7 +37,7 @@ def get_first_name_of_season_winner(data, season)
   data[season].each do |contestants|
     contestants.each do |k,v|
       if v == "Winner"
-        full_name == contestants["name"]
+        full_name = contestants["name"]
         return full_name.split(" ").first
       end
     end
