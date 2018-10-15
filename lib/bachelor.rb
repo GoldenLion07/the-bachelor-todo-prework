91a6e55e791fa0206e54ff1696fc1bf978639376
Scott M <gmat191@gmail.com>
2018-10-15 04:02:32 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -75,7 +75,7 @@ def get_occupation(data, hometown)
   data.each do |season, arr|
     arr.each do |hash|
       hash.each do |k,v|
-        
+        if v == hometown
 end
 
 def get_average_age_for_season(data, season)
